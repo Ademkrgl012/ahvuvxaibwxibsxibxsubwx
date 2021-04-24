c637656af832fa3c3081d9b81a0aed0c1d9c4c03
Glitch (joyous-rainy-knight) <none>
2021-04-24 19:11:17 +0000

    🍰🌪 Checkpoint ./bot.js:31079087/961 ./komutlar1/oynat.js:31079087/4567 ./ayarlar.json:31079087/18 ./sistem/music.js:31079087/1940

--- bot.js
+++ bot.js
@@ -39,6 +39,45 @@ client.on("ready", () => {
   );
 });
 
+client.on("warn", info => console.log(info));
+
+client.on("error", console.error)
+
+client.commands = new discord.Collection()
+client.prefix = PREFIX
+client.queue = new Map();
+
+
+const cmdFiles = readdirSync(join(__dirname, "komutlar1")).filter(file => file.endsWith(".js"))
+for (const file of cmdFiles) {
+  const command = require(join(__dirname, "komutlar1", file))
+  client.commands.set(command.name, command)
+} 
+
+
+client.on("message", message => {
+   if (message.author.bot) return;
+  if (!message.guild) return;
+  
+  if(message.content.startsWith(PREFIX)) {
+    
+    const args = message.content.slice(PREFIX.length).trim().split(/ +/)
+    const command = args.shift().toLowerCase();
+    
+    if(!client.commands.has(command)) {
+      return;
+    } 
+  try  { 
+      client.commands.get(command).execute(client, message, args)
+    } catch (err) { 
+      console.log(err)
+      message.reply("I am getting error on using this command")
+    }
+    
+  }
+  
+});
+
 //-------------Bot Eklenince Bir Kanala Mesaj Gönderme Komutu ---------------\\
 
 const emmmmbed = new Discord.MessageEmbed()
