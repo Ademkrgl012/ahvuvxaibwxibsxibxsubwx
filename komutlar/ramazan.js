733f1e93a022d5dc981239228554758e44ec3cea
Glitch (joyous-rainy-knight) <none>
2021-04-24 17:38:47 +0000

    ⚓️🐧 Checkpoint ./komutlar/ramazan.js:31079087/213

--- komutlar/ramazan.js
+++ komutlar/ramazan.js
@@ -9,12 +9,12 @@ const db = new JsonDatabase("myDatabase");
 exports.run = async (client, message, args) => {
         var yazi = args[0];
         if (!yazi) return message.channel.send(new Discord.MessageEmbed().setColor("RED").setTitle("Bir şehir girin."));
-        var yazi = yazi.toLowerCase();
+        var yazi = yazi.toUpperCase()
         var options = {
           "method": "GET",
           "hostname": "api.collectapi.com",
           "port": null,
-          "path": "/pray/all?data.city=" + yazi.toLowerCase(),
+          "path": "/pray/all?data.city=" + yazi.toUpperCase(),
           "headers": {
             "content-type": "application/json",
             "authorization": "apikey 0uQpICpHMdPOYdvARiwaSO:3LJFjzgGx7BC1qnJzUfmN5"
@@ -33,7 +33,7 @@ exports.run = async (client, message, args) => {
             res.on("end", function() {
               var body = Buffer.concat(chunks);
               let json = JSON.parse(body);
-              message.channel.send(new Discord.MessageEmbed().setColor('RED').setDescription(`${yazi}\n\nSahur: ${json.result[0].saat}\nİftar: ${json.result[4].saat}`).setFooter(`Adem Reyzz Bot Hayırlı Ramazanlar Diler.`));
+message.channel.send(new Discord.MessageEmbed().setDescription(`${yazi}\n\nSahur:${json.result[0].saat}\nİftar:${json.result[4].saat}`).setColor('RED').setFooter(`Adem Reyzz Bot Hayırlı Ramazanlar Diler.`));
               });
               });
               }catch(err){
