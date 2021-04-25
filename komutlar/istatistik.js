3973f2c64e8333db2abf76ed55482f779f295bc0
Glitch (joyous-rainy-kn) <none>
2021-04-25 02:22:56 +0000

    🗻🌊 Checkpoint ./komutlar/istatistik.js:31079087/1512 ./komutlar/avatar.js:31079087/2

--- komutlar/istatistik.js
+++ komutlar/istatistik.js
@@ -3,14 +3,14 @@ const moment = require("moment");
 const os = require('os');
 require("moment-duration-format");
 exports.run = async (bot, message, args) => {
-   const ademreyzzzaman = moment.duration(bot.uptime).format(" D [gün], H [saat], m [dakika], s [saniye]");
-   const ademreyzzistatistikler = new Discord.MessageEmbed()
+   const plasmiczaman = moment.duration(bot.uptime).format(" D [gün], H [saat], m [dakika], s [saniye]");
+   const plasmicistatistikler = new Discord.MessageEmbed()
   .setColor("RANDOM")
   .setFooter('Plasmic Code', bot.user.avatarURL)
   .addField("BOTUNUZUN ADI **İstatistikleri**", "Sunucunuz İçin En İyisi")
-  .addField("**Botun Geliştiricisi**",  " <@564837933912293386>")
+  .addField("**Botun Geliştiricisi**",  " <@>")
   .addField("**Bellek kullanımı**", (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2) + ' MB', true)  
-  .addField("**Çalışma süresi**", ademreyzzzaman)
+  .addField("**Çalışma süresi**", plasmiczaman)
   .addField("**Kullanıcılar**" , bot.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString(), true)
   .addField("**Sunucular**", bot.guilds.size.toLocaleString(), true)
   .addField("**Kanallar**", bot.channels.size.toLocaleString(), true)
@@ -19,7 +19,7 @@ exports.run = async (bot, message, args) => {
   .addField("**CPU**", `\`\`\`md\n${os.cpus().map(i => `${i.model}`)[0]}\`\`\``)
   .addField("**Bit**", `\`${os.arch()}\``, true)
   .addField("**Destek Sunucusu**", "", )
- return message.channel.send(ademreyzzistatistikler);
+ return message.channel.send(plasmicistatistikler);
   };
 
 exports.conf = {
