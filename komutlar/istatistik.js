1a17e1ddc965d52e3343e67a3db194dc27b7b18f
Glitch (joyous-rainy-kn) <none>
2021-04-25 02:23:11 +0000

    Rewound to commit 755f2264e00ceca60cabe6834e274ca77a99c073 + 0 edits

--- komutlar/istatistik.js
+++ komutlar/istatistik.js
@@ -1,36 +1,43 @@
 const Discord = require("discord.js");
-const moment = require("moment");
-const os = require('os');
-require("moment-duration-format");
-exports.run = async (bot, message, args) => {
-   const plasmiczaman = moment.duration(bot.uptime).format(" D [gün], H [saat], m [dakika], s [saniye]");
-   const plasmicistatistikler = new Discord.MessageEmbed()
-  .setColor("RANDOM")
-  .setFooter('Plasmic Code', bot.user.avatarURL)
-  .addField("BOTUNUZUN ADI **İstatistikleri**", "Sunucunuz İçin En İyisi")
-  .addField("**Botun Geliştiricisi**",  " <@>")
-  .addField("**Bellek kullanımı**", (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2) + ' MB', true)  
-  .addField("**Çalışma süresi**", plasmiczaman)
-  .addField("**Kullanıcılar**" , bot.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString(), true)
-  .addField("**Sunucular**", bot.guilds.size.toLocaleString(), true)
-  .addField("**Kanallar**", bot.channels.size.toLocaleString(), true)
-  .addField("**Discord.JS sürüm**", "v"+Discord.version, true)
-  .addField("**Ping**", bot.ping+" ms", true)
-  .addField("**CPU**", `\`\`\`md\n${os.cpus().map(i => `${i.model}`)[0]}\`\`\``)
-  .addField("**Bit**", `\`${os.arch()}\``, true)
-  .addField("**Destek Sunucusu**", "", )
- return message.channel.send(plasmicistatistikler);
-  };
+
+exports.run = (client, message) => {
+
+let motion = new Discord.MessageEmbed()
+  
+
+    .addField(
+      "Veriler", 
+      `>  Toplam sunucu: **${
+        client.guilds.cache.size
+      }** \n>  Toplam kullanıcı: **${client.guilds.cache
+        .reduce((a, b) => a + b.memberCount, 0)
+        .toLocaleString()}** \n>  Toplam kanal: **${
+        client.channels.cache.size
+      }**`
+    ) 
+    .addField(
+      "Bot Geliştiricisi",
+      `> Bot geliştiricisi : <@564837933912293386>`
+    ) 
+    .addField(
+      "Sürümler",
+      `>  Discord.js sürümü: **v${Discord.version}** \n>  Node.js sürümü: **${process.version}**`
+    ) 
+    
+    .setTimestamp()
+    .setColor("RANDOM");
+  message.channel.send(motion);
+};
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: [ 'i', 'istatistik'],
-  permLevel: 0
+  permLevel: 0,
+  aliases: ["bot-bilgi", "i"]
 };
 
 exports.help = {
   name: "istatistik",
-  description: "Bot i",
-  usage: "istatistik"
+  description: "Türkiyenin Saatini Gösterir ",
+  usage: "gç"
 };
