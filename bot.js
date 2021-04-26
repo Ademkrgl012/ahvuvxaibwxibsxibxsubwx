6acafdedf96f559ddf46487da1ea153f93649712
Glitch (joyous-rainy-kn) <none>
2021-04-26 12:40:10 +0000

    ⚽️🍢 Checkpoint ./bot.js:31079087/408

--- bot.js
+++ bot.js
@@ -2412,6 +2412,13 @@ client.on("guildMemberAdd", async member => {
 module.exports = (client, Tags, Embed, Discord) => {
 
 client.on('guildMemberAdd', async member => {
-
+  
+  const tag = await Tags.findOne({ where: { guild_id: member.guldi.id } });
+  const data = tag.get("welcome_message");
+  
+  if (data.enabled) {
+    const channel = member.guild.channels.cache.get(data.channel_id);
+    if (!channel) return;
+    const text = data.message.replace('%kullanıcı%', member.displayName).replace("%toplam
 //////////////////////
 client.login(process.env.Token);
