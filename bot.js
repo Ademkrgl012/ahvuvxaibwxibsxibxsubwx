d588456e14f16dc01a9331c7bce12c735c8d7786
Glitch (joyous-rainy-kn) <none>
2021-04-28 23:48:34 +0000

    👦🎽 Checkpoint ./bot.js:31079087/122014

--- bot.js
+++ bot.js
@@ -1872,7 +1872,7 @@ client.on("guildMemberAdd", async member => {
   const ctx = canvas.getContext("2d");
 
   const background = await Canvas.loadImage(
-     "https://cdn.discordapp.com/attachments/813881989778112562/836824107340660776/PicsArt_04-28-07.39.20.jpg"
+    "https://cdn.discordapp.com/attachments/813881989778112562/836824107340660776/PicsArt_04-28-07.39.20.jpg"
   );
   ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
 
@@ -1896,7 +1896,7 @@ client.on("guildMemberAdd", async member => {
   ctx.lineWidth = 0;
   ctx.fill();
   ctx.lineWidth = 0;
-  ctx.arc(310, 175, 100, 0, Math.PI * 2, true)
+  ctx.arc(310, 175, 100, 0, Math.PI * 2, true);
   ctx.clip();
   ctx.drawImage(avatar, 210, 75, 200, 200);
 
