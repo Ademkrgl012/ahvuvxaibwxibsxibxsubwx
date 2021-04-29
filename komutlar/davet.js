ff1615fa530b26d8522c4b5088442414e00c526d
Glitch (joyous-rainy-kn) <none>
2021-04-29 16:21:10 +0000

    🔑🦄 Checkpoint ./komutlar/davet.js:31079087/325

--- komutlar/davet.js
+++ komutlar/davet.js
@@ -8,7 +8,7 @@ exports.run = (client, message) => {
   .setColor("GOLD")
     .addField("» **Botun Sahibi**", "<@!564837933912293386>")
     .addField("**» Destek Sunucusu**", " [Sunucumuza Katıl](https://discord.gg/uruvQjX47v)", )
-    .addField("**» Davet Linki**", " [Botu Davet](https://discord.com/oauth2/authorize?client_id=826508857463275542&scope=bot&permissions=2147483647)",)
+    .addField("**» Davet Linki**", " [Botu Davet Et](https://discord.com/api/oauth2/authorize?client_id=826508857463275542&permissions=8&redirect_uri=https%3A%2F%2Fdiscord.com%2Fapi%2Foauth2%2Fauthorize%3Fclient_id%3D826508857463275542%26permissions%3D8%26scope%3Dbot&scope=bot)",)
     .setImage("https://cdn.discordapp.com/attachments/813881349004984370/826793395677691924/350kb_1.gif")
   .setFooter(`${message.author.tag} Tarafından İstendi.`, message.author.avatarURL())
   message.channel.send(chrome);   //DevTR
