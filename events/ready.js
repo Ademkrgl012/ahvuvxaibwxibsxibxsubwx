dc4f0be278a65be31aa2b1c62b693570cd79da13
Glitch (joyous-rainy-kn) <none>
2021-05-02 01:40:05 +0000

    🐀🎏 Checkpoint ./events/ready.js:31079087/2083 ./bot.js:31079087/10168

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,5 @@
+//READY.JS YE ATILACAK
+
 const chalk = require('chalk');
 const moment = require('moment');
 const Discord = require('discord.js');
@@ -6,15 +8,15 @@ var prefix = ayarlar.prefix;
 module.exports = client => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
-  client.user.setStatus("idle");
+  client.user.setStatus("online");
    var oyun = [
         "Destek sunucumuz saldırıya uğradı sunucumuza gelmek için g!desteksunucum yazabilirsiniz",
         "Destek sunucumuz saldırıya uğradı sunucumuza gelmek için g!desteksunucum yazabilirsiniz",
         "💪 7/24 Aktif!",  
-        "💡 a!davet | Botumuzu ekleyin",
-        "👨 2500 Kullanıcı!",
-        "🌍 10 Sunucuda Hizmet!",
-        "a!yardım 🔥 + a!davet 🔥 + a!otorol"
+        "💡 g!davet | Botumuzu ekleyin",
+        "👨 35.000 Kullanıcı!",
+        "🌍 115 Sunucuda Hizmet!",
+        "g!yardım 🔥 + g!davet 🔥 + g!otorol"
     ];
     setInterval(function() {
         var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
