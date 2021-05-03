89730f5f70f7ea7b5b1b553605a8462688ab77b9
Glitch (joyous-rainy-kn) <none>
2021-05-03 04:02:04 +0000

    ⏳🍦 Checkpoint ./komutlar/eğlence.js:31079087/260 ./komutlar/eğlence2.js:31079087/1485

--- komutlar/eğlence.js
+++ komutlar/eğlence.js
@@ -4,29 +4,28 @@ exports.run = async (client, message, args) => {
   let yardım = new Discord.MessageEmbed()
     .setColor("RANDOM")
     .addField("Adem Reyzz Moderasyon Menüsü",`
-✨ **a!fbi** : FBİ Gelir.
-✨ **a!token** : Botun Tokenini Görürsünüz.
-✨ **a!atam** : Dene ve Gör Reis.
-✨ **a!vine** : Komik Paylaşımlar.
-✨ **a!kasaaç**: Csgo Kasası Açar.
-✨ **a!sor**: Bota Soru Sorduğunuzda Cevap Verir.
-✨ **a!yaş-hesapla**: Yaşınızı Hesaplar.
-✨ **a!zar-at**: Zar Atarsınız.
-✨ **a!kutuaç**: Brawl Starsda Kutu Açarsınız(Uyarı Bu Oyun Amacçlı Yapılmıştır.)
-✨ **a!cihaz**: Etiket Attığınız Kişilerin Discorda Hangi Cihazla Giriş Yaptıklarını Gösterir.
-✨ **a!güzelsözler**: Size Karşı Güzel Sözler Söyler.
-✨ **a!sayı-tahmin**: Ragele Bir Sayı Tutar Ve Sizde Bulmaya Çalışırsınız.
-✨ **a!söyle**: Yazdığınız Şeyi Sesli Olarak Söyler.
-✨ **a!g-çeviri**: Yazdığınız Kelimeyi Çeviri.
-✨ **a!balıktut**: Balık Köpeğisi Tutarsınız.
-✨ **a!öp [etiket]**: Etiketlediğiniz Kişiyi Öpersiniz.
-✨ **a!trump**: Trumpa Mesaj Gönderirsiniz.
-✨ **a!espri**: Size Espri Söyler.
-✨ **a!yumruk-at**: Etiketlediğiniz Kişiye Yumruk Atar.`)
+✨ **m!fbi** : FBİ Gelir.
+✨ **m!token** : Botun Tokenini Görürsünüz.
+✨ **m!atam** : Dene ve Gör Reis.
+✨ **m!vine** : Komik Paylaşımlar.
+✨ **m!kasaaç**: Csgo Kasası Açar.
+✨ **m!sor**: Bota Soru Sorduğunuzda Cevap Verir.
+✨ **m!yaş-hesapla**: Yaşınızı Hesaplar.
+✨ **m!zar-at**: Zar Atarsınız.
+✨ **m!kutuaç**: Brawl Starsda Kutu Açarsınız(Uyarı Bu Oyun Amacçlı Yapılmıştır.)
+✨ **m!cihaz**: Etiket Attığınız Kişilerin Discorda Hangi Cihazla Giriş Yaptıklarını Gösterir.
+✨ **m!güzelsözler**: Size Karşı Güzel Sözler Söyler.
+✨ **m!sayı-tahmin**: Ragele Bir Sayı Tutar Ve Sizde Bulmaya Çalışırsınız.
+✨ **m!söyle**: Yazdığınız Şeyi Sesli Olarak Söyler.
+✨ **m!g-çeviri**: Yazdığınız Kelimeyi Çeviri.
+✨ **m!balıktut**: Balık Köpeğisi Tutarsınız.
+✨ **m!öp [etiket]**: Etiketlediğiniz Kişiyi Öpersiniz.
+✨ **m!trump**: Trumpa Mesaj Gönderirsiniz.
+✨ **m!espri**: Size Espri Söyler.
+✨ **m!yumruk-at**: Etiketlediğiniz Kişiye Yumruk Atar.`)
     .addField("**» Davet Linki**", " [Botu Davet Et](https://discord.com/oauth2/authorize?client_id=826508857463275542&scope=bot&permissions=2147483647)", )
     .setImage("https://cdn.discordapp.com/attachments/813881349004984370/827103972581048340/standard_1.gif")
-    .setFooter(`${message.author.tag} Tarafından İstendi.`,message.author.avatarURL())
-    .setFooter(
+    .setFooter(`Sayfa 2 m!eğlence2`)
     .setThumbnail(client.user.avatarURL());
   message.channel.send(yardım);
 };
@@ -34,7 +33,7 @@ exports.run = async (client, message, args) => {
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ["moderasyon"],
+  aliases: [""],
   permLevel: 0
 };
 
