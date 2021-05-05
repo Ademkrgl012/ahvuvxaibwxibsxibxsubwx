67750fa24876272c44bd1435452e9449e160364a
Glitch (joyous-rainy-kn) <none>
2021-05-05 03:06:36 +0000

    🏆🌥 Checkpoint ./komutlar/çal.js:31079087/452

---
+++ çal.js
@@ -0,0 +1,13 @@
+const play = require('discordjs-ytdl')
+module.exports = {
+  kod: "çal",
+  async run (client, message, args){
+    if (message.member.voice.channel){
+      const connection = await message.member.voice.channel.join()
+      play.play(connection, args.join(" "), '')
+      }else{
+        message.reply('Bir Sesli Kanala Katlmalısın')
+        }
+    }
+  }
+                
