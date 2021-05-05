4705341f11b269509a804273c8322611656f30bb
Glitch (joyous-rainy-kn) <none>
2021-05-05 01:23:29 +0000

    🏎🎭 Checkpoint ./events/message.js:31079087/569

--- events/message.js
+++ events/message.js
@@ -32,4 +32,21 @@ module.exports = message => {
     if (perms < cmd.conf.permLevel) return;
     cmd.run(client, message, params, perms);
   }
-};
+// if (cmd) {
+//
+// } 
+// en sondaki süslü parantezden sonra yapıştırın.
+//
+// string-similarity modülü gereklidir. konsola      npm i string-similarity yazarak    indirebilirsiniz.
+
+ else{
+    const laura = [];
+    client.commands.forEach(dropinnem => {
+      laura.push(dropinnem.help.name);
+      dropinnem.conf.aliases.forEach(abcdef => laura.push(abcdef));
+    });
+
+    const rifleman = require('string-similarity').findBestMatch(command, laura);
+    message.channel.send(`bunu mu demek istediniz? ${rifleman.bestMatch.target}`)
+  };
+  };
