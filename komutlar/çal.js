73003bca71d97fc32b564083a5c1d89bcf37a632
Glitch (joyous-rainy-kn) <none>
2021-05-07 22:33:09 +0000

    🐵🔔 Checkpoint ./komutlar/çal.js:31079087/176

--- komutlar/çal.js
+++ komutlar/çal.js
@@ -8,11 +8,11 @@ const { MessageEmbed } = require('discord.js')
       .setFooter(message.author.tag, message.author.displayAvatarURL({dynamic: true}))
       const connection = await message.member.voice.channel.join()
       play.play(connection, args.join(" "), 'AIzaSyDRvGKGjL8b-lAiSkFmv3azTchsEcrOK5w')
-      let title = play.title(args.join(" "), 'AIzaSyDRvGKGjL8b-lAiSkFmv3azTchsEcrOK5w
+      let title = play.title(args.join(" "), 'AIzaSyDRvGKGjL8b-lAiSkFmv3azTchsEcrOK5w')
 title.then(titlee => embed.addField('Video Başlığı: ', titlee))
-let kanal = play.channel(args.join(" "), 'AIzaSyADEGjdpN0qGUp855hODYZrjlMvEg4JZT4')
+let kanal = play.channel(args.join(" "), 'AIzaSyDRvGKGjL8b-lAiSkFmv3azTchsEcrOK5w')
 kanal.then(titlee => embed.addField('Kanal Adı: ', titlee))
-let id = play.id(args.join(" "), 'AIzaSyADEGjdpN0qGUp855hODYZrjlMvEg4JZT4')
+let id = play.id(args.join(" "), 'AIzaSyDRvGKGjL8b-lAiSkFmv3azTchsEcrOK5w')
 id.then(titlee => embed.addField('Video Idsi: ', titlee))
 setTimeout(function () {
   message.channel.send(embed)
@@ -23,7 +23,7 @@ setTimeout(function () {
     };
 exports.conf = {
   enabled: true,
-  guildOnly: true,
+  guildOnly: false,
   aliases: [],
   permLevel: 0
   };
