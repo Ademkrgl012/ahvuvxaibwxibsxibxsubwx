ccf169559b5fd96cb4f0f5dec4eb8b23eb76c4b2
Glitch (joyous-rainy-kn) <none>
2021-05-07 19:53:24 +0000

    🐃🏪 Checkpoint ./komutlar/çal.js:31079087/695

--- komutlar/çal.js
+++ komutlar/çal.js
@@ -10,4 +10,15 @@ module.exports = {
       .setFooter(message.author.tag, message.author.displayAvatarURL({dynamic: true}))
       const connection = await message.member.voice.channel.join()
       play.play(connection, args.join(" "), 'AIzaSyADEGjdpN0qGUp855hODYZrjlMvEg4JZT4')
-      
+      let title = play.title(args.join(" "), 'AIzaSyADEGjdpN0qGUp855hODYZrjlMvEg4JZT4')
+title.then(titlee => embed.addField('Video Başlığı: ', titlee))
+let kanal = play.channel(args.join(" "), 'AIzaSyADEGjdpN0qGUp855hODYZrjlMvEg4JZT4')
+kanal.then(titlee => embed.addField('Kanal Adı: ', titlee))
+let id = play.id(args.join(" "), 'AIzaSyADEGjdpN0qGUp855hODYZrjlMvEg4JZT4')
+id.then(titlee => embed.addField('Video Idsi: ', titlee))
+setTimeout(function
+      } else {
+        message.reply('Bir Kanala Katılmaısın')
+        }
+    }
+  
